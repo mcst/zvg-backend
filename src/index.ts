afe4c1758f51bb292cca9baaea48327c91de01c9
mcst <marcelstuewe@gmail.com>
2021-12-16 22:15:10 +0100

    Alle immobilien pro Gericht suchen

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,80 @@
-import { JSDOM } from 'jsdom';
+import {JSDOM} from 'jsdom';
 import fetch from 'node-fetch';
 
+type KeyValue = { [key: string]: string | number };
+const GERICHTE: KeyValue = {
+    BERGHEIM: "R3302",
+    BOCHUM: "R2201",
+    CASTROP: "R2401",
+    DORTMUND: "R2402",
+    ESSEN: "R2503",
+    KOELN: "R3306"
+};
+
+const OBJEKTE: number[] = [1, 2, 3, 19, 4];
+const OBJEKT_LISTE: number = 4;
 
 (async () => {
-  const host = 'http://www.zvg-portal.de/';
-  //const {default: fetch} = await import('node-fetch');
-  const res = await fetch(host + 'index.php?button=Suchen', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'ger_name=Bochum&order_by=2&land_abk=nw&ger_id=R2201&az1=&az2=&az3=&az4=&art=&obj=&str=&hnr=&plz=&ort=&ortsteil=&vtermin=&btermin='
-  });
-
-  const {window} = new JSDOM(await res.text());
-  const doc = window.document;
-
-  const table = doc.querySelector('#inhalt form table') as HTMLTableElement;
-  const rows = table.querySelectorAll('tr');
-
-  const urls = [];
-  for (let i = 0; i < rows.length; i += 7) {
-    const row = rows[i];
-    const link = row.querySelector('a');
-    urls.push(host + link.href);
-  }
-
-  console.log(urls);
-
-
-  const res2 = await fetch(urls[0], {
-    headers: {
-      'Referer': host + 'index.php?button=Suchen'
+    const getObjekte = (Objekte:number[]) => {
+        return Objekte.map((nr,index, list)=>(`obj_arr%5B%5D=${nr}${index<list.length-1?"&":""}`)).join("");
     }
-  });
-  console.log(await res2.text());
+    const host = 'http://www.zvg-portal.de/';
+    const searchList:string[] = Object.keys(GERICHTE).map(key => `ger_name=${key}&ger_id=${GERICHTE[key]}&${getObjekte(OBJEKTE)}&obj_liste=${OBJEKT_LISTE}&order_by=2&land_abk=nw&az1=&az2=&az3=&az4=&art=&obj=&str=&hnr=&plz=&ort=&ortsteil=&vtermin=&btermin=`);
+
+    console.log(searchList);
+
+    const sleep = async(duration:number) =>{
+        return new Promise(res=>{
+           setTimeout(()=>res(true),duration);
+        });
+    }
+    function getRandomInt(min:number, max:number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+
+
+    const getUrl = (table:HTMLTableElement) =>{
+        const rows= table?.querySelectorAll('tr');
+
+        const url = [];
+        for(let row of rows){
+            const link = row?.querySelector('a');
+            const {href} = link||{};
+            if(href?.includes("index.php")) {
+                url.push(host +link?.href);
+            }
+        }
+        return url;
+    }
+
+    const checkResponse = (htmlText:string) => {
+        const {window} = new JSDOM(htmlText);
+        const doc = window.document;
+        const tables = doc.querySelectorAll('table');
+        const midTable = tables[1];
+        const table = midTable ?? tables[0];
+        console.log(getUrl(table));
+    }
+    for(let searchString of searchList){
+        const res = await fetch(host + 'index.php?button=Suchen&all=1', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: searchString
+        });
+        await sleep(getRandomInt(500,2000));
+        console.log(searchString);
+        checkResponse(await res.text());
+    }
+
+    // const res2 = await fetch(urls[0], {
+    //     headers: {
+    //         'Referer': host + 'index.php?button=Suchen'
+    //     }
+    // });
+    // console.log(await res2.text());
 })();
